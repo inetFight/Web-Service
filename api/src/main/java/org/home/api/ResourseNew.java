57ccc67cf3ba121a5095e774bd430f7fafef1658
inetFight <d-litvinov@ukr.net>
2016-02-15 18:22:01 +0200

    1.2

---
+++ api/src/main/java/org/home/api/ResourseNew.java
@@ -0,0 +1,55 @@
+package org.home.api;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import user.xml.User;
+import user.xml.UserService;
+
+
+
+@Path("/balance/new/")
+public class ResourseNew {
+UserService service = new UserService();
+	@GET
+	@Path("/{username}")
+	@Produces(MediaType.APPLICATION_XML)
+	public List<User> getMessage(@PathParam("username") String username) {
+		List<User> list = new ArrayList<>();
+		if(service.checkUser(username)){
+		 list.add(service.getUser(username));
+		}
+		return list;
+	}
+
+	@PUT
+	@Path("/{username}/{balance}")
+	@Produces(MediaType.APPLICATION_XML)
+	public User getMessage(@PathParam("username") String username, @PathParam("balance") int balance) {
+		if(service.checkUser(username)){
+			service.addBalance(username, balance);
+			return service.getUser(username);
+		}
+		else {
+			service.addUser(username, balance);
+			return service.getUser(username);
+		}
+	}
+
+	@GET
+	@Path("/")
+	@Produces(MediaType.APPLICATION_XML)
+	public List<User> getUsers() {
+		return service.getAllUsers();
+	}
+	
+
+   
+}
